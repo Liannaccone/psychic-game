7e665dac1a10b2211ef7abf33122ccd220aacb02
liannaccone <lciannaccone@gmail.com>
2018-01-22 22:31:16 -0800

    adding completed js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,19 +16,37 @@
 	// user has 9 attempts to correctly guess the computerLetter
 	var guessesLeft = 9;
 	var guessedLetters = []
+	var userGuess = null
+
+	console.log("computerLetter: " + computerLetter + ", winsCount: " + winsCount + ", lossesCount: " + lossesCount + ", guessesLeft: " + guessesLeft + ", guessedLetters: " + guessedLetters)
 
 //  FUNCTIONS
 // ===================================
 
 	// refreshes #wrongGuesses with the updated array
 	var refreshWrongGuesses = function() {
-		document.getElementById("#wrongGuesses").innterHTML = "It's not " + guessedLetters.join(", or ");	
+		document.querySelector("#wrongGuesses").innerHTML = "Your guesses so far:  " + guessedLetters.join(", ");	
 	}
 
 	//  refreshes #guessesLeft after each attempt
 	var refreshGuessesLeft = function() {
-		document.getElementById("#guessesLeft").innterHTML = "Guesses Left: " + guessesLeft;
+		document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
 		}
+
+	// refreshes #wins when called
+	var refreshWins = function() {
+		document.querySelector("#wins").innerHTML = "Wins: " + winsCount;
+	}
+
+	// refreshes computerLetter variable, randomly assigning another value from the alphabet array
+	var refreshComputerLetter = function() {
+  		this.computerLetter = this.alphabet[Math.floor(Math.random() * this.alphabet.length)];
+ 	}
+
+	// refreshes #losses when called
+	var refreshLosses = function() {
+		document.querySelector("#losses").innerHTML = "Losses: " + lossesCount;
+	}
 
 
 	// reset function to be used when guesses = 0 || userGuess = computerLetter
@@ -37,17 +55,71 @@
 		guessedLetters = [];
 		refreshWrongGuesses();
 		refreshGuessesLeft();
+		refreshComputerLetter();
 
 	}
 
 
 
+	// MAIN PROCESS
+	// ==================================
+
+	// refreshGuessesLeft();
+	refreshComputerLetter();
 
 
+	// When user presses a key it runs the following function:
+	document.onkeyup = function(event) {
+		var userGuess = String.fromCharCode(event.which).toLowerCase();
+		// decreases the number of guesses left by 1
 
-	}
+		if (guessedLetters.indexOf(userGuess) !== -1) {
+			alert("You already guessed " + userGuess + " silly!!!");
+			return;
+		}
+
+		if (alphabet.indexOf(userGuess) === -1) {
+			alert("That's not even a letter, try again!");
+			return;
+		}
+
+		guessesLeft--;
+
+		guessedLetters.push(userGuess);
+		console.log(guessedLetters);
 
 
+			// losing scenario, out of guesses
+		if (guessesLeft === 0) {
+			// increase lossesCount by 1
+			lossesCount++;
+			// alerts user of loss
+			alert("Out of guesses! Try again?");
+			// refresh the #losses div with updated lossesCount
+			refreshLosses();
+			// resets variables back to 0
+			reset();
+
+		}
+			// winning scenario, correct guess
+		if (userGuess == computerLetter) {
+			// increase wins count by 1
+			winsCount++;
+			// alerts user of win
+			alert("You got it! I was thinking " + computerLetter + "!")
+			// refreshes the #wins div with updated winsCount
+			refreshWins();
+			// resets variables back to 0
+			reset();
+		}
+
+		else {
+			refreshWrongGuesses();
+			refreshGuessesLeft();
+		}
+
+
+	};
 
 
 
